38deb6ca6a70994c20fa06d5b86edb20b964ea7c
Prokyonn <daniel@sulu.io>
2024-04-30 11:25:31 +0200

    Add CI and linting tools

---
+++ Tests/test-bootstrap.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of Sulu.
+ *
+ * (c) Sulu GmbH
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+\date_default_timezone_set('UTC');
+
+require __DIR__ . '/Application/config/bootstrap.php';
