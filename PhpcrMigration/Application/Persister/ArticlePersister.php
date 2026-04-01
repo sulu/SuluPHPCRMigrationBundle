5278a3ead8ba4dc1377aa3614657db78c3c2f04b
Martin Lagler <martin@sulu.io>
2026-04-01 15:05:54 +0200

    Title too long exception

--- PhpcrMigration/Application/Persister/ArticlePersister.php
+++ PhpcrMigration/Application/Persister/ArticlePersister.php
@@ -11,6 +11,7 @@
 
 namespace Sulu\Bundle\PhpcrMigrationBundle\PhpcrMigration\Application\Persister;
 
+use Sulu\Bundle\PhpcrMigrationBundle\PhpcrMigration\Application\Exception\TitleTooLongException;
 use Sulu\Bundle\PhpcrMigrationBundle\PhpcrMigration\Application\Repository\EntityRepositoryInterface;
 use Symfony\Component\PropertyAccess\PropertyAccessorInterface;
 
@@ -57,8 +58,14 @@ class ArticlePersister extends AbstractPersister
         $data['workflowPlace'] = 2 === ($data['workflowPlace'] ?? null) ? 'published' : 'draft';
 
         if (isset($data['title'])) {
-            $data['title'] = \str_split((string) $data['title'], 191)[0];
-            $data['templateData']['title'] = $data['title'];
+            $title = (string) $data['title'];
+
+            if (\strlen($title) > 191) {
+                throw new TitleTooLongException($title, $document['jcr']['uuid'], $locale);
+            }
+
+            $data['title'] = $title;
+            $data['templateData']['title'] = $title;
         }
 
         if (null !== $locale && isset($document['localizations'][$locale])) {
--- PhpcrMigration/Application/Persister/SnippetPersister.php
+++ PhpcrMigration/Application/Persister/SnippetPersister.php
@@ -11,6 +11,7 @@
 
 namespace Sulu\Bundle\PhpcrMigrationBundle\PhpcrMigration\Application\Persister;
 
+use Sulu\Bundle\PhpcrMigrationBundle\PhpcrMigration\Application\Exception\TitleTooLongException;
 use Sulu\Bundle\PhpcrMigrationBundle\PhpcrMigration\Application\Repository\EntityRepositoryInterface;
 use Symfony\Component\PropertyAccess\PropertyAccessorInterface;
 
@@ -50,8 +51,14 @@ class SnippetPersister extends AbstractPersister
         $data['workflowPlace'] = 'published';
 
         if (isset($data['title'])) {
-            $data['title'] = \str_split((string) $data['title'], 191)[0];
-            $data['templateData']['title'] = $data['title'];
+            $title = (string) $data['title'];
+
+            if (\strlen($title) > 191) {
+                throw new TitleTooLongException($title, $document['jcr']['uuid'], $locale);
+            }
+
+            $data['title'] = $title;
+            $data['templateData']['title'] = $title;
         }
 
         // Snippets store template as unlocalized property (same template for all locales)
