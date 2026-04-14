b1b2b84acd03bc5ea27177eae0dd5ac16509f5ff
Prokyonn <daniel.mathis@sector8.eu>
2026-04-14 09:07:24 +0200

    Fix missing provider key in linkData migration (#45)
    Fix linkData migration

--- PhpcrMigration/Application/Persister/PagePersister.php
+++ PhpcrMigration/Application/Persister/PagePersister.php
@@ -171,6 +171,7 @@ class PagePersister extends AbstractPersister
             $targetUuid = $localization['internal_link'] ?? null;
             $data['linkProvider'] = 'page'; // Sulu 2.6 did only support 'page' as internal link provider
             $data['linkData'] = null !== $targetUuid ? [
+                'provider' => 'page',
                 'href' => $targetUuid,
                 'locale' => $locale,
             ] : null;
@@ -179,6 +180,7 @@ class PagePersister extends AbstractPersister
             $externalUrl = $localization['external'] ?? null;
             $data['linkProvider'] = 'external';
             $data['linkData'] = null !== $externalUrl ? [
+                'provider' => 'external',
                 'href' => $externalUrl,
                 'locale' => $locale,
             ] : null;
